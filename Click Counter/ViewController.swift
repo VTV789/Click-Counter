2759e0bb284a022c984d8f9ffa78f9ac063c5453
Vinh Thanh Vu <vtv@vinh.com>
2015-08-03 23:36:19 -0400

    add button and label
    Lesson 1/5: Outlets & Actions > Coding Click Counter
    
    creating a label and button
    1) setting the button frame
    2) setting the title and giving it a color
    3) adding a the button view as a sub-view or our rear view

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -12,10 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
      
+        //Label
         var label = UILabel()
         label.frame = CGRectMake(150, 150, 60, 60)//use the method CGRectMake to set the X&Y cordinat and also the hight and width
         label.text = "0"
         
         self.view.addSubview(label)
+        
+        //Button
+        var button = UIButton()
+        button.frame = CGRectMake(150, 250, 60, 60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
     }
 }
