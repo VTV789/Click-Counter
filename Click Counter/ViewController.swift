44c797b127fa2e45a5eb329afdf564749926a8fb
Vinh Thanh Vu <vtv@vinh.com>
2015-08-04 23:58:52 -0400

    Change the button name
    Lesson 1/14: Outlets & Actions > Coding Click Counter

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -11,27 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     
     var count = 0
-    var label: UILabel! //the "!" indicates implicit
+    @IBOutlet var label: UILabel! //the "!" indicates implicitly unwrap optional
+    
     override func viewDidLoad() {
         super.viewDidLoad()
      
-        //Label
-        var label = UILabel()
-        label.frame = CGRectMake(150, 150, 60, 60)//use the method CGRectMake to set the X&Y cordinat and also the hight and width
-        label.text = "0"
-        
         self.view.addSubview(label)
-        self.label = label // self.label
-        
-        //Button
-        var button = UIButton()
-        button.frame = CGRectMake(150, 250, 60, 60)
-        button.setTitle("Click", forState: .Normal)
-        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        self.view.addSubview(button)
-        
-        button.addTarget(self, action: "incrementCount", forControlEvents:
-            UIControlEvents.TouchUpInside)
     }
     
     func incrementCount(){
