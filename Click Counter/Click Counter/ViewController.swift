731762820f402b2e34e7347d0708953df4b965f5
Vinh Thanh Vu <vtv@vinh.com>
2015-11-29 22:42:52 -0500

    Add  a click button

--- Click Counter/Click Counter/ViewController.swift
+++ Click Counter/Click Counter/ViewController.swift
@@ -11,9 +11,21 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {             // This execute once after the inital view object is set
         super.viewDidLoad()
+        
+        //Label
         let label = UILabel()
         label.frame = CGRectMake(150, 150, 60, 60)
         label.text = "0"
+        
+        self.view.addSubview(label)
+        
+        //Button
+        let button = UIButton()
+        button.frame = CGRectMake(150, 250, 60, 60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
     }
 }
+
 
