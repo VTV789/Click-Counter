811ce75f376de37555b53b4a384820bdfc44ee29
Vinh Thanh Vu <vtv@vinh.com>
2015-12-05 17:58:01 -0500

    Delete hard code (label and button) to recreate it in the storyboard

--- Click Counter/Click Counter/ViewController.swift
+++ Click Counter/Click Counter/ViewController.swift
@@ -16,23 +16,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {             // This execute once after the inital view object is set
         super.viewDidLoad()
         
-        //Label
-        let label = UILabel()
-        label.frame = CGRectMake(150, 150, 60, 60)
-        label.text = "0"
-        
-        self.view.addSubview(label)
-        self.label = label                    // Setting the label text when the button is clicked
-        
-        //Button
-        let button = UIButton()
-        button.frame = CGRectMake(150, 250, 60, 60)
-        button.setTitle("Click", forState: .Normal)
-        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        self.view.addSubview(button)
-        
-        button.addTarget(self, action: "incrementCount", forControlEvents:
-            UIControlEvents.TouchUpInside)
     }
     
     func incrementCount(){                   // This method will be called when the button is clicked
